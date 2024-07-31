ff476bd13d306d83a84ac945a55961ee519d76a6
Serhan Apaydın <zerhan@gmail.com>
2024-07-31 12:38:50 +0300

    Convert every single instance of "_Copy" to "Co_py" in order to release the "C" key accelerator

--- thunar/thunar-dnd.c
+++ thunar/thunar-dnd.c
@@ -69,7 +69,7 @@ thunar_dnd_ask (GtkWidget    *widget,
                 GdkDragAction dnd_actions)
 {
   static const GdkDragAction dnd_action_items[] = { GDK_ACTION_COPY, GDK_ACTION_MOVE, GDK_ACTION_LINK };
-  static const gchar        *dnd_action_names[] = { N_ ("_Copy here"), N_ ("_Move here"), N_ ("_Link here") };
+  static const gchar        *dnd_action_names[] = { N_ ("Co_py here"), N_ ("_Move here"), N_ ("_Link here") };
   static const gchar        *dnd_action_icons[] = { "stock_folder-copy", "stock_folder-move", NULL };
 
   ThunarxProviderFactory *factory;
